cca40cd47e6736d0b06270366262c049df55a6b8
Shelly <shelly.sri18@gmail.com>
2026-04-08 11:57:15 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        // for(int i = 0;i<nums.size();i++){
+        //     for(int j = i + 1;j<nums.size();j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        unordered_map<int,int>mpp;
+        for(int i = 0;i<nums.size();i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
