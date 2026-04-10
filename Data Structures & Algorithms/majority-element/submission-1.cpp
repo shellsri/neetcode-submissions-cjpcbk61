7c9426def20578d1856f8dda1be96b390c8772e6
Shelly <shelly.sri18@gmail.com>
2026-04-10 22:08:13 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        int res =0;
+        for(int i = 0;i<nums.size();i++){
+             mpp[nums[i]]++;
+             if(mpp[nums[i]]>nums.size()/2){
+                res = nums[i];
+             }
+        }
+        return res;
+        
+    }
+};
