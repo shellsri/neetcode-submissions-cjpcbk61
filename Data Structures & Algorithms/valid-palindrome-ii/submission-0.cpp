7a27c352a25bda84d009dc8f2b16c4b4ffe90201
Shelly <shelly.sri18@gmail.com>
2026-04-12 17:28:16 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPal(string s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            if(s[i] != s[j]){
+                return isPal(s,i+1,j) || isPal(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+};
