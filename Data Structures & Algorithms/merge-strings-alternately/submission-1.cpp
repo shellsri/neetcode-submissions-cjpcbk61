a64dc3c95174c9ea4ee9bc192e500a6475304796
Shelly <shelly.sri18@gmail.com>
2026-04-12 17:51:01 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        // int n = word1.size()+word2.size();
+        // string word(n,' ');
+        // int i = 0;
+        // int j = 0;
+        // for(int k = 0;k<n;k++){
+        //     if(k %2 ==0 && i<word1.size() || j>=word2.size()){
+        //         word[k] =word1[i++];
+        //     }else{
+        //         word[k] =word2[j++];
+        //     }
+            
+        // }
+        // return word;
+        int i = 0;
+        int j = 0;
+        string word="";
+        while(i<word1.size()||j<word2.size()){
+            if(i<word1.size())word +=word1[i++];
+            if(j<word2.size())word +=word2[j++];
+        }
+        return word;
+    }
+};
